18283dc45e5f08fefa0077f72dfd31f020c6f0c1
dungnt11 <33595434+dungnt11@users.noreply.github.com>
2020-02-07 02:42:10 +0700

    Update passion.js

--- passion.js
+++ passion.js
@@ -1,21 +1,34 @@
-$(document).ready(() => {
-   const flag = confirm("Có muốn download hình không anh zai?");
-    if (flag) {
-        const wrapper = $('.separator');
-        let listLink = [];
-        Array.from(wrapper).forEach(item => {
-            for(let img of item.children) {
-                listLink.push(img.href);
-            }
-        });
-        listLink.forEach(item => {
-            var link = document.createElement('a');
-            link.href = item;
-            link.target = "_blank";
-            link.download = Math.random() + '.jpg';
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        });
-    } 
-}) 
+/*
+ * @author Nguyen Dung 
+ * @create 07/02/2020
+ */
+!!(() => {
+   /**
+    * (c)https://stackoverflow.com/questions/17527713/force-browser-to-download-image-files-on-click
+    */
+   const forceDownload = (url, fileName) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "blob";
+    xhr.onload = function(){
+        var urlCreator = window.URL || window.webkitURL;
+        var imageUrl = urlCreator.createObjectURL(this.response);
+        var tag = document.createElement('a');
+        tag.href = imageUrl;
+        tag.download = fileName;
+        document.body.appendChild(tag);
+        tag.click();
+        document.body.removeChild(tag);
+    }
+    xhr.send();
+   }
+     let imgList = [];
+      (Array.from($('.separator'))).forEach(item => {
+          Array.from(item.querySelectorAll('img')).forEach(img => imgList.push(img.src))
+      });
+     imgList.forEach(item => {
+          setInterval(() => {
+            forceDownload(item, Math.random() + '.jpg');
+         }, 0); 
+      });
+})();
