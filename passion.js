9c5384a798e520f466175b0e5a7bccbc5de675ff
dungnt11 <33595434+dungnt11@users.noreply.github.com>
2020-02-07 03:27:09 +0700

    Update passion.js

--- passion.js
+++ passion.js
@@ -2,33 +2,31 @@
  * @author Nguyen Dung 
  * @create 07/02/2020
  */
-!!(() => {
-   /**
-    * (c)https://stackoverflow.com/questions/17527713/force-browser-to-download-image-files-on-click
-    */
-   const forceDownload = (url, fileName) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.responseType = "blob";
-    xhr.onload = function(){
-        var urlCreator = window.URL || window.webkitURL;
-        var imageUrl = urlCreator.createObjectURL(this.response);
-        var tag = document.createElement('a');
-        tag.href = imageUrl;
-        tag.download = fileName;
-        document.body.appendChild(tag);
-        tag.click();
-        document.body.removeChild(tag);
+!(async () => {
+  const toDataURL = (url) => {
+    return fetch(url).then((response) => {
+      return response.blob();
+    }).then(blob => {
+      return URL.createObjectURL(blob);
+    });
+  }
+
+  const download = async (url, nameFile) => {
+    const a = document.createElement("a");
+    a.href = await toDataURL(url);
+    a.download = nameFile;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+
+  let imgList = [];
+  (Array.from($('.separator'))).forEach(item => {
+    Array.from(item.querySelectorAll('img')).forEach(img => imgList.push(img.src))
+  });
+  for (const item of imgList) {
+    if (/(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))/.test(item)) {
+      await download(item, item.slice(item.lastIndexOf('/') + 1));
     }
-    xhr.send();
-   }
-     let imgList = [];
-      (Array.from($('.separator'))).forEach(item => {
-          Array.from(item.querySelectorAll('img')).forEach(img => imgList.push(img.src))
-      });
-     imgList.forEach(item => {
-          setInterval(() => {
-            forceDownload(item, Math.random() + '.jpg');
-         }, 0); 
-      });
+  }
 })();
